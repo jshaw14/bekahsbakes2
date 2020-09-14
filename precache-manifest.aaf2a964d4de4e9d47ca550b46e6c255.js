75a13251b493ed32494e913f718bca647bc190f9
J Shaw <jordan14shaw@gmail.com>
2020-09-14 19:11:56 +0100

    Updates

---
+++ precache-manifest.aaf2a964d4de4e9d47ca550b46e6c255.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620a98555df710e255a87edd75973eba",
+    "url": "/jshaw14/bekahsbakes/index.html"
+  },
+  {
+    "revision": "e3365485e85e7ecb3c3b",
+    "url": "/jshaw14/bekahsbakes/static/js/2.abfd2f52.chunk.js"
+  },
+  {
+    "revision": "44159dcb838322a1f17355bda9441bd7",
+    "url": "/jshaw14/bekahsbakes/static/js/2.abfd2f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e30cd9774ab80b76090",
+    "url": "/jshaw14/bekahsbakes/static/js/main.a6f61102.chunk.js"
+  },
+  {
+    "revision": "2ec26e7e72eae3bfa99f",
+    "url": "/jshaw14/bekahsbakes/static/js/runtime-main.7ee8f198.js"
+  },
+  {
+    "revision": "b1433032edfbdef413cf66a012f9b526",
+    "url": "/jshaw14/bekahsbakes/static/media/BVCenReport.b1433032.pdf"
+  },
+  {
+    "revision": "997aecd4f5cd310f2f47f65b96ebf806",
+    "url": "/jshaw14/bekahsbakes/static/media/Dissertation.997aecd4.pdf"
+  },
+  {
+    "revision": "22c3ae7f2ddd10e28d7f7700e99bf598",
+    "url": "/jshaw14/bekahsbakes/static/media/EDFAFinalReport.22c3ae7f.pdf"
+  },
+  {
+    "revision": "a22aada123f9040e48d8a5dc09e384c5",
+    "url": "/jshaw14/bekahsbakes/static/media/chocolatecake.a22aada1.jpg"
+  },
+  {
+    "revision": "0d91cc396656376349da7ef60ce6d894",
+    "url": "/jshaw14/bekahsbakes/static/media/facebook.0d91cc39.svg"
+  },
+  {
+    "revision": "a5cbd922d588921d3df950f25978268d",
+    "url": "/jshaw14/bekahsbakes/static/media/geometric.a5cbd922.jpg"
+  },
+  {
+    "revision": "98273ce42863a73b61249dd08dac97f4",
+    "url": "/jshaw14/bekahsbakes/static/media/gintheme.98273ce4.jpg"
+  },
+  {
+    "revision": "942df1e77191e071fa7b3c4b80ddd909",
+    "url": "/jshaw14/bekahsbakes/static/media/heroimage.942df1e7.jpg"
+  },
+  {
+    "revision": "5ed981b8a4420ab6780b97dd6458f838",
+    "url": "/jshaw14/bekahsbakes/static/media/insta.5ed981b8.png"
+  },
+  {
+    "revision": "2a775dc238439f52de330bc15a675445",
+    "url": "/jshaw14/bekahsbakes/static/media/weddingcake.2a775dc2.jpg"
+  }
+]);
